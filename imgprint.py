8fb28c5c3f6957aecb7799d13c888720a4c97685
Talon Daniels <talondaniels@gmail.com>
2017-01-16 22:10:42 -0700

    Imports

--- imgprint.py
+++ imgprint.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-from tprinter import *
+import platform
 if 'arm' in platform.machine():
     import Image
 else:
     from PIL import Image
+from tprinter import *
 
 printer = ThermalPrinter()
 printer.set_defaults()
