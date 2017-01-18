25c360116997bac9f99039603268d32050e6889c
Talon Daniels <talondaniels@gmail.com>
2017-01-17 20:15:57 -0700

    Add font size and write commands

--- twitchprinter.py
+++ twitchprinter.py
@@ -22,7 +22,10 @@ class PrinterBot(irc.bot.SingleServerIRCBot):
         printer.set_defaults()
         self.logtime = int(time.time())
         self.gen = imagegen.ImageGenerator()
-        self.font = ImageFont.truetype('fonts/hack-bold.ttf', 24)
+        self.font_name = 'fonts/hack-bold.ttf'
+        self.font_size = 24
+        self.font = ImageFont.truetype(self.font_name, self.font_size)
+        self.handwriting_font = ImageFont.truetype('fonts/talonscript.ttf', 32)
 
     def on_welcome(self, c, e):
         print('Joining channel {}'.format(self.channel))
@@ -42,9 +45,10 @@ class PrinterBot(irc.bot.SingleServerIRCBot):
             print('Uh oh!')
             traceback.print_exc()
 
-    def append_log(self, log):
-        with open('log.txt', 'a') as fd:
-            fd.write('{}\n'.format(log))
+    def update_font(self, name=None, size=None):
+        self.font_name = name if name is not None else self.font_name
+        self.font_size = size if size is not None else self.font_size
+        self.font = ImageFont.truetype(self.font_name, self.font_size)
 
     def handle_message(self, c, e):
         msg = e.arguments[0]
@@ -52,15 +56,23 @@ class PrinterBot(irc.bot.SingleServerIRCBot):
         record.log_message(nick, msg)
         now = datetime.datetime.now().strftime('%c')
         log = '[{}] {}: {}'.format(now, e.source.nick, msg)
-        print(log)
         cmd = msg.split(' ')[0].lower()
         arg = msg.replace(cmd + ' ', '')
         if cmd == 'print':
             img = self.gen.render_string(arg, self.font)
             printer.print_image(img)
-        if cmd == 'image':
+        elif cmd == 'write':
+            img = self.gen.render_string(arg, self.handwriting_font)
+            printer.print_image(img)
+        elif cmd == 'image':
             img = self.gen.render_image_code(arg)
             printer.print_image(img)
+        elif cmd == 'large':
+            self.update_font(size=36)
+        elif cmd == 'medium':
+            self.update_font(size=28)
+        elif cmd == 'small':
+            self.update_font(size=24)
         elif cmd == 'moar':
             if nick in admins:
                 printer.linefeed(1)
@@ -70,13 +82,13 @@ class PrinterBot(irc.bot.SingleServerIRCBot):
                 printer.wake()
                 printer.set_defaults()
 
-        self.append_log(log)
-        
         now = int(time.time())
         if now - self.logtime > 30 * 60:
             printer.set_defaults()
             self.logtime = now
             printer.linefeed(1)
+
+        print(log)
 
 
 def main():
