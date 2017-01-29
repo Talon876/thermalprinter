9fef1499ce2c5a1b0fb887ce8ebd99c886739358
Talon Daniels <talondaniels@gmail.com>
2017-01-29 00:58:02 -0700

    Use main method

--- tprinter/imgprint.py
+++ tprinter/imgprint.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
+from argparse import ArgumentParser
 import platform
 if 'arm' in platform.machine():
     import Image
 else:
     from PIL import Image
-from tprinter import *
+from tprinter import ThermalPrinter
 
-printer = ThermalPrinter()
-printer.set_defaults()
-saved_img = Image.open('img.bmp')
-printer.print_image(saved_img)
+def main():
+    printer = ThermalPrinter()
+    printer.set_defaults()
+    saved_img = Image.open('img.bmp')
+    printer.print_image(saved_img)
+
+if __name__=='__main__':
+    main()
+
