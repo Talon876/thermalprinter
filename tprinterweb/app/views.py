45eff5eca6d6abb932ada418de2c6b00be9cd916
Talon Daniels <talondaniels@gmail.com>
2017-02-04 13:46:32 -0700

    Add login page

--- tprinterweb/app/views.py
+++ tprinterweb/app/views.py
@@ -3,9 +3,44 @@ import datetime as dt
 
 from app import app, db
 from .auth import OAuthSignIn
+from .models import User
+
+@app.before_request
+def before_request():
+    g.user = None
 
 @app.route('/')
 def index():
     return render_template('index.html',
             title='Home')
+
+@app.route('/login')
+def login():
+    providers = [
+        { 'name': 'twitch', 'button_img': 'https://i.imgur.com/f9H7QoX.png' }
+    ]
+    return render_template('login.html', title='Login', providers=providers)
+
+@app.route('/authorize/<provider>')
+def authorize(provider):
+    oauth = OAuthSignIn.get_provider(provider)
+    return oauth.authorize()
+
+@app.route('/callback/<provider>')
+def oauth_callback(provider):
+    oauth = OAuthSignIn.get_provider(provider)
+    social_username = oauth.callback()
+    if social_username is None:
+        flash('Login failed')
+        return redirect(url_for('login'))
+
+    social_id = '{}/{}'.format(provider, social_username)
+    user = User.query.filter_by(social_id=social_id).first()
+    if user is None:
+        user = User(social_id=social_id)
+        app.logger.info('Adding user {} to database'.format(social_id))
+        db.session.add(user)
+        db.session.commit()
+    app.logger.info('Authorized {}'.format(user))
+    return redirect(url_for('index'))
 
